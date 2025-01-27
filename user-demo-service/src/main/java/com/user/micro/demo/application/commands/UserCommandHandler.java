dd36a835c1fd236e017ce0bc71aa4a38e9831542
Carlos Nunes <carlos.nunes@pt.softinsa.com>
2025-01-27 12:21:27 +0000

    Update logic to include logging;

--- user-demo-service/src/main/java/com/user/micro/demo/application/commands/UserCommandHandler.java
+++ user-demo-service/src/main/java/com/user/micro/demo/application/commands/UserCommandHandler.java
@@ -10,6 +10,8 @@ import com.user.micro.demo.exception.InsufficientFundsException;
 import com.user.micro.demo.exception.UserNotFoundException;
 import com.user.micro.demo.infrastructure.repository.UserRepository;
 import jakarta.transaction.Transactional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -50,6 +52,11 @@ public class UserCommandHandler {
 
     @Transactional
     public UserDto ExecuteTransaction(Long userId, CreateTransactionCommand request){
+        Logger logger = LoggerFactory.getLogger(this.getClass());
+
+        logger.info("Executing transaction for userId={}, amount={}, type={}",
+                userId, request.amount, request.type);
+
         User user = this.userRepository.findById(userId)
                 .orElseThrow(() -> new UserNotFoundException("No such user with id: " + userId.toString()));
 
@@ -63,12 +70,15 @@ public class UserCommandHandler {
             default -> throw new IllegalArgumentException("Invalid transaction type");
         }
 
+        logger.info("Calling transaction service to create transaction");
         TransactionDto transaction = proxy.createTransaction(userId, request);
+        logger.info("Received transaction response: {}", transaction);
 
         user.addTransactionToTransactionList(transaction);
-
         user.setBalance(user.getBalance() + request.amount);
         this.userRepository.save(user);
+
+        logger.info("Transaction executed successfully and user updated");
 
         return this.userMapper.toDto(user);
     }
